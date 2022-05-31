dc81e6a17952384f3fa2728f8dd6ab1a5619eafa
dlfud <99ysl@naver.com>
2022-05-31 12:20:28 +0900

    about페이지 추가

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function AboutPage(){
+    return(
+        <main>
+            <titile>자기소개</titile>
+            <h1>자기소개 페이지 입니다.</h1>
+        </main>
+    );
+}
+
+export default AboutPage;
