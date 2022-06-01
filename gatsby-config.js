ae739fdee4ceee26a3896204a787bb3ed05db3df
dlfud <99ysl@naver.com>
2022-06-01 16:15:12 +0900

    각 페이지에 이미지 넣음

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
-    description:"이 사이트는 개발일지가 담겨있습니다.",
+    description: "이 사이트는 개발일지가 담겨있습니다.",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
-      }
+      },
     },
     "gatsby-plugin-mdx",
   ],
