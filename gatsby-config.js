ac8ca042b60e24ef0628de65b44dd11396f65a02
dlfud <99ysl@naver.com>
2022-06-01 10:51:25 +0900

    소개글 넣기...?

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
-module.exports = {
-  siteMetadata: {
     title: "My First Gatsby Site",
+    description:"이 사이트는 개발일지가 담겨있습니다.",
   },
   plugins: [
     "gatsby-plugin-image",
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
   navLinkText,
+  siteTitle,
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query{
+        site{
+            siteMetadata{
+                title
+                description
+            }
+        }
+    }
+  `)
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <div>{data.site.siteMetadata.description}</div>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
