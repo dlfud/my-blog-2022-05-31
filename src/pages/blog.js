46eafce46a8ceb967f445d86ec0b5789a6d30468
dlfud <99ysl@naver.com>
2022-06-01 12:35:13 +0900

    구성요소 사용해서 블로그 페이지에서 게시

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,37 @@
 import React from 'react'
 import Layout from "../components/Layout"
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({data}) => {
     
     return (
         <Layout pageTitle="My Blog Page">
-            <ul>
             {
-                data.allFile.nodes.map(node => (
-                    <li key={node.name}>
-                        {node.name}
-                    </li>
+                data.allMdx.nodes.map((node) => (
+                <article key={node.id}>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                    <MDXRenderer>
+                        {node.body}
+                    </MDXRenderer>
+                </article>
                 ))
             }
-            </ul>
         </Layout>
     );
 }
 
 export const query = graphql`
-    query{
-        allFile{
-            nodes{
-                name
+    query {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+            nodes {
+                frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+                }
+                id
+                body
             }
         }
     }
