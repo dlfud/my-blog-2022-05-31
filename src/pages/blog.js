86c13e9073db1cff08377ce6dc3da8f2a8339232
dlfud <99ysl@naver.com>
2022-06-01 11:50:24 +0900

    MDX블로그 게시물 작성

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import Layout from "../components/Layout"
+import { graphql } from 'gatsby'
 
-function BlogPage(){
+const BlogPage = ({data}) => {
+    
     return (
         <Layout pageTitle="My Blog Page">
-
+            <ul>
+            {
+                data.allFile.nodes.map(node => (
+                    <li key={node.name}>
+                        {node.name}
+                    </li>
+                ))
+            }
+            </ul>
         </Layout>
     );
 }
+
+export const query = graphql`
+    query{
+        allFile{
+            nodes{
+                name
+            }
+        }
+    }
+`
 
 export default BlogPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,6 @@ const IndexPage = () => {
       <h1>HomePage</h1>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="../images/image1.jpg"
       />
     </Layout>
